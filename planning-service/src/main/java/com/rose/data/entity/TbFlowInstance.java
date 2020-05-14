9852861e323acd446a8b7e22e5df95f15b52594b
sunp <sunp@dcnoahark.com>
2020-05-14 13:50:55 +0800

    2020

--- planning-service/src/main/java/com/rose/data/entity/TbFlowInstance.java
+++ planning-service/src/main/java/com/rose/data/entity/TbFlowInstance.java
@@ -43,6 +43,14 @@ public class TbFlowInstance extends BaseDataIdLong {
     @Column(name = "state", columnDefinition = "TINYINT DEFAULT 0 COMMENT '流程实例状态'")
     private Integer state;
 
+    // 正在处理的流程实例节点id
+    // 注意：空：流程已经完成，没有需要操作的流程节点
+    //      当前正在处理的节点，可能有一个，也可能有多个
+    //      比如，流转三个节点 a、b、c，则值为 aId,bId,cId
+    //            而a 已处理完成后，则值变为 bId,cId
+    @Column(name = "handing_instance_node_id", columnDefinition = "VARCHAR(255) COMMENT '当前正在处理的流程实例节点id'")
+    private String handingInstanceNodeId;
+
     //@Transient
     //private List<TbFlowInstanceOperateHistory> operateHistory;
 }
