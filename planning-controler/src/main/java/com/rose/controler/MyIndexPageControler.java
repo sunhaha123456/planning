9334b6e518b234119b09f60d4762b97f55fe37b5
sunp <sunp@dcnoahark.com>
2020-05-24 16:16:23 +0800

    开始我的首页

--- planning-controler/src/main/java/com/rose/controler/MyIndexPageControler.java
+++ planning-controler/src/main/java/com/rose/controler/MyIndexPageControler.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 描述：首页 controller
+ * 描述：我的首页 controller
  * @author sunpeng
  */
 @Slf4j
 @RestController
-@RequestMapping("/user/indexPage")
-public class IndexPageControler {
+@RequestMapping("/user/myIndexPage")
+public class MyIndexPageControler {
 
 }
