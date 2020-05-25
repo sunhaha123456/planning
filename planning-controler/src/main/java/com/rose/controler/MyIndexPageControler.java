7290394072dcb2841fab1406cc5abbe7391292e8
sunp <sunp@dcnoahark.com>
2020-05-25 16:20:09 +0800

    系统优化

--- planning-controler/src/main/java/com/rose/controler/MyIndexPageControler.java
+++ planning-controler/src/main/java/com/rose/controler/MyIndexPageControler.java
@@ -1,17 +1,23 @@
 package com.rose.controler;
 
 import com.rose.common.data.base.PageList;
+import com.rose.common.data.response.ResponseResultCode;
+import com.rose.common.exception.BusinessException;
 import com.rose.data.base.PageParam;
 import com.rose.data.entity.TbFlowInstance;
+import com.rose.data.entity.TbFlowInstanceOperateHistory;
 import com.rose.data.entity.TbNotice;
 import com.rose.data.entity.TbSystemSetting;
+import com.rose.data.to.request.FlowInstanceRequest;
 import com.rose.data.to.request.NoticeSearchRequest;
+import com.rose.data.to.response.FlowChartResponse;
 import com.rose.service.FlowInstanceService;
 import com.rose.service.PublishNoticeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 描述：我的首页 controller
@@ -48,5 +54,32 @@ public class MyIndexPageControler {
         return flowInstanceService.searchWaitingApproval(param);
     }
 
+    /**
+     * 功能：获取流程实例详情
+     * @param id
+     * @param attachFileFlag 0不返回附件信息 1返回附件信息
+     * @return
+     */
+    @GetMapping(value= "/getFlowInstanceDetail")
+    public TbFlowInstance getFlowInstanceDetail(@RequestParam Long id, @RequestParam(required = false, defaultValue = "0") Integer attachFileFlag) {
+        return flowInstanceService.getFlowInstanceDetail(id, attachFileFlag, null);
+    }
 
+    @PostMapping(value= "/getOperateInfo")
+    public PageList<TbFlowInstanceOperateHistory> getOperateInfo(@RequestBody FlowInstanceRequest param) throws Exception {
+        if (param == null || param.getId() == null) {
+            throw new BusinessException(ResponseResultCode.PARAM_ERROR);
+        }
+        return flowInstanceService.getOperateInfo(param);
+    }
+
+    @GetMapping(value= "/getFlowInstanceFlowChart")
+    public FlowChartResponse getFlowInstanceFlowChart(@RequestParam Long id) {
+        return flowInstanceService.getFlowInstanceFlowChart(id, null);
+    }
+
+    @GetMapping(value= "/exportFileFlowInstance")
+    public void exportFileFlowInstance(HttpServletResponse resp, @RequestParam Long instanceId, @RequestParam Long fileId) throws Exception {
+        flowInstanceService.exportFileFlowInstance(resp, instanceId, fileId, null);
+    }
 }
