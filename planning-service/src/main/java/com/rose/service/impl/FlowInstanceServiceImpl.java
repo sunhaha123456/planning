39b0affb994b557fea5c683c532a3983e53f45c3
sunp <sunp@dcnoahark.com>
2020-05-26 10:45:26 +0800

    系统优化

--- planning-service/src/main/java/com/rose/service/impl/FlowInstanceServiceImpl.java
+++ planning-service/src/main/java/com/rose/service/impl/FlowInstanceServiceImpl.java
@@ -630,6 +630,7 @@ public class FlowInstanceServiceImpl implements FlowInstanceService {
         if (instanceId == null || !Arrays.asList(0, 1).contains(approvalApplyOperateType) || StringUtil.isEmpty(approvalApplyContent)) {
             throw new BusinessException(ResponseResultCode.PARAM_ERROR);
         }
+        Long userId = valueHolder.getUserIdHolder();
         TbFlowInstance instance = flowInstanceRepository.findOne(instanceId);
         if (instance.getState() != FlowInstanceStateEnum.HAVE_STARTED.getIndex()) {
             throw new BusinessException("不能对已审核的申请再次审核！");
@@ -669,8 +670,11 @@ public class FlowInstanceServiceImpl implements FlowInstanceService {
             if (nodeTaskListTemp == null || nodeTaskListTemp.size() == 0) {
                 throw new BusinessException(ResponseResultCode.OPERT_ERROR);
             }
+            for (TbFlowInstanceNodeUserTask t : nodeTaskListTemp) {
+                if (t.getState() == FlowInstanceNodeUserTaskStateEnum.WAITINT_OPERATE.getIndex()) {
 
-
+                }
+            }
         }
 
 
