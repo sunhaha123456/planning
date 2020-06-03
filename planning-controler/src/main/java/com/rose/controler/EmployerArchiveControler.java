8d2f44aaca0362b2db186dc07c91f245f49c0ae5
sunp <sunp@dcnoahark.com>
2020-06-03 19:02:40 +0800

    系统优化

---
+++ planning-controler/src/main/java/com/rose/controler/EmployerArchiveControler.java
@@ -0,0 +1,42 @@
+package com.rose.controler;
+
+import com.rose.common.data.base.PageList;
+import com.rose.data.entity.TbEmployer;
+import com.rose.data.to.request.EmployerSearchRequest;
+import com.rose.service.EmployerService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import javax.inject.Inject;
+
+/**
+ * 描述：人员档案 controller
+ * @author sunpeng
+ */
+@Slf4j
+@RestController
+@RequestMapping("/user/employerArchive")
+public class EmployerArchiveControler {
+
+    @Inject
+    private EmployerService employerService;
+
+    @Value("${file.upload.maxSize}")
+    private Long fileMaxSize;
+
+    @PostMapping(value= "/search")
+    public PageList<TbEmployer> search(@RequestBody EmployerSearchRequest param) throws Exception {
+        return employerService.search(param);
+    }
+
+    @GetMapping(value= "/getEmployer")
+    public TbEmployer getEmployer(@RequestParam Long id) throws Exception {
+        return employerService.getEmployer(id, true);
+    }
+
+    @PostMapping(value= "/updateEmployerSalaryPosition")
+    public void updateEmployerSalaryPosition(@RequestBody TbEmployer param) {
+        employerService.updateEmployerSalaryPosition(param);
+    }
+}
